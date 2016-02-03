71afee846a2e1cecde43b46410d3f69f59ba9db7
jakdillard <jakdillard@gmail.com>
2016-02-03 12:29:56 -0500

    don't build launcher3

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -41,5 +41,4 @@ BUILD_FINGERPRINT="google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys" 
 PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
 
 PRODUCT_PACKAGES += \
-    Launcher3
 
