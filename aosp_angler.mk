5fee2e2c880420a269229b4449708bcdbd24930e
jakdillard <jakdillard@gmail.com>
2016-02-06 01:00:35 -0500

    yeah build launcher3

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -41,6 +41,7 @@ BUILD_FINGERPRINT="google/angler/angler:6.0.1/MMB29Q/2480792:user/release-keys" 
 PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29Q 2480792 release-keys"
 
 PRODUCT_PACKAGES += \
-   CellBroadcastReceiver
+    CellBroadcastReceiver \
+    Launcher3
 
 
