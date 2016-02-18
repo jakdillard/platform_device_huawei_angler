51261866e311c189327c04666a326eb572d91e65
jakdillard <jakdillard@gmail.com>
2016-02-18 11:03:11 -0500

    build kernel from source (inline kernel building)

--- device.mk
+++ device.mk
@@ -20,14 +20,14 @@
 # Everything in this directory will become public
 
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/huawei/angler-kernel/Image.gz-dtb
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#LOCAL_KERNEL := device/huawei/angler-kernel/Image.gz-dtb
+#else
+#LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
 
-PRODUCT_COPY_FILES := \
-    $(LOCAL_KERNEL):kernel
+#PRODUCT_COPY_FILES := \
+#    $(LOCAL_KERNEL):kernel
 
 PRODUCT_COPY_FILES += \
     device/huawei/angler/init.angler.rc:root/init.angler.rc \
